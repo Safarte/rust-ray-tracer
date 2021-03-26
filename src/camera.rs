e7a019bcbd97799e976d28b8ab58a74854457dca
Brice Chichereau <brice.chichereau@datadoghq.com>
2021-03-26 09:21:42 +0100

    update

--- src/camera.rs
+++ src/camera.rs
@@ -1,3 +1,5 @@
+use rand::{thread_rng, Rng};
+
 use crate::{
     ray::Ray,
     vec3::{Point3, Vec3},
@@ -8,33 +10,68 @@ pub struct Camera {
     lower_left_corner: Point3,
     horizontal: Vec3,
     vertical: Vec3,
+    u: Vec3,
+    v: Vec3,
+    w: Vec3,
+    lens_radius: f64,
 }
 
 impl Camera {
-    pub fn new() -> Camera {
-        let aspect_ratio = 16. / 9.;
-        let viewport_height = 2.;
+    pub fn new(
+        lookfrom: Point3,
+        lookat: Point3,
+        vup: Vec3,
+        vfov: f64, // Vertical FoV
+        aspect_ratio: f64,
+        aperture: f64,
+        focus_dist: f64,
+    ) -> Camera {
+        let theta = vfov.to_radians();
+        let h = (theta / 2.).tan();
+        let viewport_height = 2. * h;
         let viewport_width = aspect_ratio * viewport_height;
-        let focal_length = 1.;
 
-        let origin = Point3::zero();
-        let horizontal = Vec3::new(viewport_width, 0., 0.);
-        let vertical = Vec3::new(0., viewport_height, 0.);
-        let lower_left_corner =
-            origin - (horizontal / 2.) - (vertical / 2.) - Vec3::new(0., 0., focal_length);
+        let w = (lookfrom - lookat).unit_vector();
+        let u = vup.cross(&w).unit_vector();
+        let v = w.cross(&u);
+
+        let origin = lookfrom;
+        let horizontal = focus_dist * viewport_width * u;
+        let vertical = focus_dist * viewport_height * v;
+        let lower_left_corner = origin - (horizontal / 2.) - (vertical / 2.) - focus_dist * w;
+
+        let lens_radius = aperture / 2.;
 
         Camera {
             origin,
             horizontal,
             vertical,
             lower_left_corner,
+            u,
+            v,
+            w,
+            lens_radius,
         }
     }
 
-    pub fn get_ray(&self, u: f64, v: f64) -> Ray {
+    pub fn get_ray(&self, s: f64, t: f64) -> Ray {
+        let rd = self.lens_radius * random_in_unit_disk();
+        let offset = self.u * rd.x() + self.v * rd.y();
         Ray::new(
-            self.origin,
-            self.lower_left_corner + u * self.horizontal + v * self.vertical - self.origin,
+            self.origin + offset,
+            self.lower_left_corner + s * self.horizontal + t * self.vertical - self.origin - offset,
         )
+    }
+}
+
+fn random_in_unit_disk() -> Vec3 {
+    let mut rng = thread_rng();
+    let min = -1.;
+    let max = 1.;
+    loop {
+        let p = Vec3::new(rng.gen_range(min..max), rng.gen_range(min..max), 0.);
+        if p.length_squared() < 1. {
+            return p;
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use camera::Camera;
 use geometry::{Hittable, Sphere};
 use material::{Dielectric, Lambertian, Metal};
 use rand::{thread_rng, Rng};
-use vec3::{Color, Point3};
+use vec3::{Color, Point3, Vec3};
 
 fn ray_color(ray: &ray::Ray, world: &Vec<Box<dyn Hittable>>, depth: i32) -> Color {
     if depth <= 0 {
@@ -30,53 +30,107 @@ fn ray_color(ray: &ray::Ray, world: &Vec<Box<dyn Hittable>>, depth: i32) -> Colo
     (1. - t) * Color::new(1., 1., 1.) + t * Color::new(0.5, 0.7, 1.)
 }
 
+fn random_scene() -> Vec<Box<dyn Hittable>> {
+    let mut rng = thread_rng();
+    let mut world: Vec<Box<dyn Hittable>> = Vec::new();
+
+    let ground_material = Arc::new(Lambertian {
+        albedo: Color::new(0.5, 0.5, 0.5),
+    });
+    world.push(Box::new(Sphere {
+        center: Point3::new(0., -1000., 0.),
+        radius: 1000.,
+        material: ground_material,
+    }));
+
+    let comp = Point3::new(4., 0.2, 0.);
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat: f64 = rng.gen();
+            let center = Point3::new(
+                a as f64 + 0.9 * rng.gen::<f64>(),
+                0.2,
+                b as f64 + 0.9 * rng.gen::<f64>(),
+            );
+
+            if (center - comp).length() > 0.9 {
+                if choose_mat < 0.8 {
+                    let albedo = Vec3::random_range(0., 1.);
+                    let material = Arc::new(Lambertian { albedo });
+                    world.push(Box::new(Sphere {
+                        center,
+                        radius: 0.2,
+                        material,
+                    }))
+                } else if choose_mat < 0.95 {
+                    let albedo = Vec3::random_range(0.5, 1.);
+                    let fuzziness: f64 = rng.gen_range((0.)..0.5);
+                    let material = Arc::new(Metal { albedo, fuzziness });
+                    world.push(Box::new(Sphere {
+                        center,
+                        radius: 0.2,
+                        material,
+                    }))
+                } else {
+                    let material = Arc::new(Dielectric { ir: 1.5 });
+                    world.push(Box::new(Sphere {
+                        center,
+                        radius: 0.2,
+                        material,
+                    }))
+                }
+            }
+        }
+    }
+
+    let material = Arc::new(Dielectric { ir: 1.5 });
+    world.push(Box::new(Sphere {
+        center: Point3::new(0., 1., 0.),
+        radius: 1.,
+        material,
+    }));
+
+    let material = Arc::new(Lambertian {
+        albedo: Color::new(0.4, 0.2, 0.1),
+    });
+    world.push(Box::new(Sphere {
+        center: Point3::new(-4., 1., 0.),
+        radius: 1.,
+        material,
+    }));
+
+    let material = Arc::new(Metal {
+        albedo: Color::new(0.7, 0.6, 0.5),
+        fuzziness: 0.,
+    });
+    world.push(Box::new(Sphere {
+        center: Point3::new(4., 1., 0.),
+        radius: 1.,
+        material,
+    }));
+
+    world
+}
+
 fn main() {
     // Image
-    const ASPECT_RATIO: f64 = 16. / 9.;
-    const IMAGE_WIDTH: i32 = 1280;
+    const ASPECT_RATIO: f64 = 3. / 2.;
+    const IMAGE_WIDTH: i32 = 640;
     const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i32;
-    const SAMPLES: i32 = 16;
-    const MAX_DEPTH: i32 = 10;
+    const SAMPLES: i32 = 512;
+    const MAX_DEPTH: i32 = 50;
     let mut rng = thread_rng();
 
     // World
-    let mut world: Vec<Box<dyn Hittable>> = Vec::new();
-
-    world.push(Box::new(Sphere {
-        center: Point3::new(0., -100.5, -1.),
-        radius: 100.,
-        material: Arc::new(Lambertian {
-            albedo: Color::new(0.8, 0.8, 0.),
-        }),
-    }));
-    world.push(Box::new(Sphere {
-        center: Point3::new(0., 0., -1.),
-        radius: 0.5,
-        material: Arc::new(Lambertian {
-            albedo: Color::new(0.1, 0.2, 0.5),
-        }),
-    }));
-    world.push(Box::new(Sphere {
-        center: Point3::new(-1., 0., -1.),
-        radius: -0.4,
-        material: Arc::new(Dielectric { ir: 1.5 }),
-    }));
-    world.push(Box::new(Sphere {
-        center: Point3::new(-1., 0., -1.),
-        radius: 0.5,
-        material: Arc::new(Dielectric { ir: 1.5 }),
-    }));
-    world.push(Box::new(Sphere {
-        center: Point3::new(1., 0., -1.),
-        radius: 0.5,
-        material: Arc::new(Metal {
-            albedo: Color::new(0.8, 0.6, 0.2),
-            fuzziness: 0.1,
-        }),
-    }));
+    let world = random_scene();
 
     // Camera
-    let camera = Camera::new();
+    let lookfrom = Point3::new(13., 2., 3.);
+    let lookat = Point3::new(0., 0., 0.);
+    let vup = Vec3::new(0., 1., 0.);
+    let dist_to_focus = 10.;
+    let camera = Camera::new(lookfrom, lookat, vup, 20., ASPECT_RATIO, 0.1, dist_to_focus);
 
     // Render
     println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
